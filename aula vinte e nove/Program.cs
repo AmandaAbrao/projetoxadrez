5969763c04c2b756bfb1bffab285edef88ed69e3
Amanda Abrão <amandaabrao55@gmail.com>
2016-10-03 10:21:58 -0300

    Criando a classe posiçao xadrex

--- aula vinte e nove/Program.cs
+++ aula vinte e nove/Program.cs
@@ -6,24 +6,14 @@ namespace aula_vinte_e_nove {
     class Program {
         static void Main(string[] args) {
 
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
+            Console.WriteLine(pos);
 
-            try {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.WriteLine(pos.toPosicao());
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+            Console.ReadLine();
 
-
-
-                Tela.imprimirTabuleiro(tab);
-            }
-
-            catch (TabuleiroException e) {
-                Console.WriteLine(e.Message);
-            }
-                Console.ReadLine();
-            }
+           }
          }
        }
