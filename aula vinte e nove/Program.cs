128e81d2c69ceec609026cc9ba6479d8342b491c
AMANDA SOUZA ABRAO <12919274686@est.udicentro.local>
2016-10-17 08:17:29 -0200

    Corrigindo alguns erros de compilacao junto com o professor

--- aula vinte e nove/Program.cs
+++ aula vinte e nove/Program.cs
@@ -13,23 +13,38 @@ namespace aula_vinte_e_nove {
 
                 while (!partida.terminada) {
 
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab);
+                    try {
+                        Console.Clear();
+                        Tela.imprimirPartida(partida);
+                      
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
 
-                    Console.WriteLine();
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
-                    Console.Write("Origem: ");
-                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
-                    Console.Write("Destino: ");
-                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
 
-                    partida.executaMovimento(origem, destino);
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
+
+
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e) {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
                 }
             }
             catch (TabuleiroException e) {
                 Console.WriteLine(e.Message);
             }
-                Console.ReadLine();
-            }
+            Console.ReadLine();
         }
     }
+}
--- aula vinte e nove/Tela.cs
+++ aula vinte e nove/Tela.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using tabuleiro;
 using xadrez;
 
@@ -6,25 +7,83 @@ using xadrez;
 namespace aula_vinte_e_nove {
     class Tela {
 
+
+        public static void imprimirPartida(PartidaDeXadrez partida) {
+            imprimirTabuleiro(partida.tab);
+            Console.WriteLine();
+            imprimirPecasCapturadas(partida);
+            Console.WriteLine();
+            Console.WriteLine("Turno: " + partida.turno);
+            Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+            if (partida.xeque) {
+                Console.WriteLine("XEQUE!");
+            }
+        }
+
+        public static void imprimirPecasCapturadas(PartidaDeXadrez partida) {
+            Console.WriteLine("Peças caprturadas: ");
+            Console.Write("Brancas: ");
+            imprimirConjunto(partida.pecasCapturadas(Cor.Branca));
+            Console.WriteLine();
+            Console.Write("Pretas: ");
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            imprimirConjunto(partida.pecasCapturadas(Cor.Preta));
+            Console.ForegroundColor = aux;
+            Console.WriteLine();
+
+        }
+
+
+        public static void imprimirConjunto(HashSet<Peca> conjunto) {
+            Console.Write("[");
+            foreach (Peca x in conjunto) {
+                Console.Write(x + " ");
+            }
+            Console.Write("]");
+        }
+
+
         public static void imprimirTabuleiro(Tabuleiro tab) {
 
-            for (int i = 0; i<tab.linhas; i++) {
+            for (int i = 0; i < tab.linhas; i++) {
                 Console.Write(8 - i + " ");
-                for ( int j = 0; j <tab.colunas; j++) {
-
-                    if (tab.peca(i, j) == null) {
-                        Console.Write("- ");
-                    }
-
-                    else {
-                        imprimirPeca(tab.peca(i, j));
-                        Console.Write(" ");
-
-                    }
+                for (int j = 0; j < tab.colunas; j++) {
+                    imprimirPeca(tab.peca(i, j));
                 }
                 Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h");
+        }
+
+
+        public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoesPossiveis) {
+
+            ConsoleColor fundoOriginal = Console.BackgroundColor;
+            ConsoleColor fundoAlterado = ConsoleColor.DarkGray;
+
+            for (int i = 0; i < tab.linhas; i++) {
+                Console.Write(8 - i + " ");
+                for (int j = 0; j < tab.colunas; j++) {
+                    if (posicoesPossiveis[i, j]) {
+                        Console.BackgroundColor = fundoAlterado;
+                    }
+
+                    else {
+                        Console.BackgroundColor = fundoOriginal;
+                    }
+
+                    imprimirPeca(tab.peca(i, j));
+                    Console.BackgroundColor = fundoOriginal;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("  a b c d e f g h");
+            Console.BackgroundColor = fundoOriginal;
+
         }
 
         public static PosicaoXadrez lerPosicaoXadrez() {
@@ -35,17 +94,28 @@ namespace aula_vinte_e_nove {
         }
 
         public static void imprimirPeca(Peca peca) {
-            if (peca.cor == Cor.Branca) {
-                Console.Write(peca);
+
+            if (peca == null) {
+                Console.Write("- ");
             }
+
             else {
-                ConsoleColor aux = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write(peca);
-                Console.ForegroundColor = aux;
+
+                if (peca.cor == Cor.Branca) {
+                    Console.Write(peca);
+                }
+                else {
+                    ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write(peca);
+                    Console.ForegroundColor = aux;
+
+                }
+
+                Console.Write(" ");
 
             }
-
         }
     }
 }
+
